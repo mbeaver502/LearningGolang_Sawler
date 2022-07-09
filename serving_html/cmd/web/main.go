76e6828fc8f1438c12bb6a66d1c5f9203ab3c4a9
Michael Beaver <jmbeaver1@gmail.com>
2022-07-09 13:46:23 -0500

    Add Session Management using scs Package
    Use the scs package to add session management.
    Use the AppConfig type to track the session and give access
    to the session manager
    throughout the prog.

--- serving_html/cmd/web/main.go
+++ serving_html/cmd/web/main.go
@@ -3,7 +3,9 @@ package main
 import (
 	"log"
 	"net/http"
+	"time"
 
+	"github.com/alexedwards/scs/v2"
 	"github.com/mbeaver502/LearningGolang_Sawler/serving_html/pkg/config"
 	"github.com/mbeaver502/LearningGolang_Sawler/serving_html/pkg/handlers"
 	"github.com/mbeaver502/LearningGolang_Sawler/serving_html/pkg/render"
@@ -11,14 +13,15 @@ import (
 
 const PORT_NUMBER = ":8080"
 
+var app *config.AppConfig
+
 // main is the program entrypoint.
 func main() {
-	app := setupAppConfig()
+	app = setupAppConfig()
 
-	repo := handlers.NewRepo(app)
-	handlers.NewHandlers(repo)
-
-	render.NewTemplates(app)
+	setupSession(app)
+	setupHandlers(app)
+	setupTemplates(app)
 
 	// Launch a server and start serving requests on localhost:PORT_NUMBER
 	log.Printf("Starting server on port %v\n", PORT_NUMBER)
@@ -43,7 +46,24 @@ func setupAppConfig() *config.AppConfig {
 	}
 
 	app.TemplateCache = tc
-	app.UseCache = false
+	app.UseCache = false     // change to true when in prod
+	app.InProduction = false // change to true when in prod
 
 	return &app
+}
+
+func setupSession(a *config.AppConfig) {
+	app.Session = scs.New()
+	app.Session.Lifetime = 24 * time.Hour
+	app.Session.Cookie.Persist = true
+	app.Session.Cookie.SameSite = http.SameSiteLaxMode
+	app.Session.Cookie.Secure = app.InProduction
+}
+
+func setupHandlers(a *config.AppConfig) {
+	handlers.NewHandlers(handlers.NewRepo(a))
+}
+
+func setupTemplates(a *config.AppConfig) {
+	render.NewTemplates(a)
 }
