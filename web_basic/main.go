f19eafdc53d9cc6855264e6a25f72f523e5afc01
Michael Beaver <jmbeaver1@gmail.com>
2022-07-09 10:17:03 -0500

    Add Additional Handlers
    Add Home and About handlers to process requests on the
    / and /about routes. Add Hello
    handler to process requests on
    the /hello route. Pretty neat.

--- web_basic/main.go
+++ web_basic/main.go
@@ -2,23 +2,57 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
 )
 
+const PORT_NUMBER = ":8080"
+
+// Home is the home page handler.
+// Any kind of handler func *must* take in an http.ResponseWriter and a *http.Request!
+func Home(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "This is the home page.")
+}
+
+// About is the about page handler.
+func About(w http.ResponseWriter, r *http.Request) {
+	x, y := 2, 2
+	sum := addValues(x, y)
+
+	fmt.Fprintf(w, "This is the about page. And %v + %v is %v", x, y, sum)
+}
+
+// Hello is the hello page handler.
+func Hello(w http.ResponseWriter, r *http.Request) {
+	n, err := fmt.Fprintf(w, "Hello World!")
+
+	if err != nil {
+		log.Println("hello error:", err)
+	}
+
+	log.Printf("Number of bytes written: %d; Error: %v\n", n, err)
+}
+
+// addValues adds two integers and returns the sum.
+// Notice that this is a normal function, not a request handler!
+// Our addValues func will be unexported, so only package main can access it
+func addValues(x, y int) int {
+	return x + y
+}
+
+// main is the program entrypoint.
 func main() {
-	// Create a request handler on the main root (e.g., https://localhost:8080/)
-	// Note that our handler func is an anonymous function that takes in a writer and *request
-	// In this example, we're simply printing out "Hello World!" for every request
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		n, err := fmt.Fprintf(w, "Hello World!")
+	registerHandlers()
 
-		if err != nil {
-			fmt.Println(err)
-		}
-
-		fmt.Printf("Number of bytes written: %d; Error: %v\n", n, err)
-	})
+	log.Printf("Starting server on port %v\n", PORT_NUMBER)
 
 	// Launch a server and start serving requests on localhost:8080
-	http.ListenAndServe(":8080", nil)
+	http.ListenAndServe(PORT_NUMBER, nil)
+}
+
+// registerHandlers registers all the handlers for the page routes.
+func registerHandlers() {
+	http.HandleFunc("/", Home)
+	http.HandleFunc("/about", About)
+	http.HandleFunc("/hello", Hello)
 }
