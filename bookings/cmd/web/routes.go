852c5ad6b56ca8c0e75e8d0d01e7b969b1a22570
Michael Beaver <jmbeaver1@gmail.com>
2022-07-09 15:44:22 -0500

    Serve Static Files
    Tell chi router how to server static files on HTML templates.

--- bookings/cmd/web/routes.go
+++ bookings/cmd/web/routes.go
@@ -25,5 +25,9 @@ func routes(app *config.AppConfig) http.Handler {
 	mux.Get("/", handlers.Repo.Home)
 	mux.Get("/about", handlers.Repo.About)
 
+	// tell chi how to serve static files
+	fileServer := http.FileServer(http.Dir("./static/"))
+	mux.Handle("/static/*", http.StripPrefix("/static", fileServer))
+
 	return mux
 }
