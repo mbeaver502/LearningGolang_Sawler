dbd22b120d711e0a53068abf7925c9e3460d97c4
Michael Beaver <jmbeaver1@gmail.com>
2022-07-09 11:52:04 -0500

    Update Render to Use Template Cache
    Use a map as a cache for page templates.
    Removes need for server to re-render every page per request.

--- serving_html/pkg/render/render.go
+++ serving_html/pkg/render/render.go
@@ -1,14 +1,17 @@
 package render
 
 import (
+	"fmt"
 	"log"
 	"net/http"
 	"strings"
 	"text/template"
 )
 
+var templateCache = make(map[string]*template.Template)
+
 // renderTemplate renders an HTML template to the given http.ResponseWriter.
-func RenderTemplate(w http.ResponseWriter, tmpl string) {
+func RenderTemplateOld(w http.ResponseWriter, tmpl string) {
 	const layoutPath = "./templates/base.layout.tmpl"
 	templatePath := strings.Join([]string{"./templates/", tmpl}, "")
 
@@ -19,4 +22,49 @@ func RenderTemplate(w http.ResponseWriter, tmpl string) {
 		log.Println("renderTemplate error:", err)
 		return
 	}
+}
+
+// renderTemplate renders an HTML template to the given http.ResponseWriter.
+func RenderTemplate(w http.ResponseWriter, t string) {
+	var tmpl *template.Template
+	var err error
+
+	// check if we already have the rendered template in templateCache
+	if _, ok := templateCache[t]; !ok {
+		// need to create the template
+		err = createTemplateCache(t)
+
+		if err != nil {
+			log.Println(err)
+		}
+	} else {
+		// we already have template cached
+		log.Println("using cached template", t)
+	}
+
+	tmpl = templateCache[t]
+	err = tmpl.Execute(w, nil)
+
+	if err != nil {
+		log.Println(err)
+	}
+}
+
+func createTemplateCache(t string) error {
+	templates := []string{
+		fmt.Sprintf("./templates/%s", t),
+		"./templates/base.layout.tmpl",
+	}
+
+	// parse the template
+	tmpl, err := template.ParseFiles(templates...)
+
+	if err != nil {
+		return err
+	}
+
+	// add template to cache
+	templateCache[t] = tmpl
+
+	return nil
 }
