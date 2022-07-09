2b4da401cd36a3a7fa924c5f7f116a714d307be8
Michael Beaver <jmbeaver1@gmail.com>
2022-07-09 13:14:09 -0500

    Replace pat with chi
    Set up routing using chi package instead of pat.

--- serving_html/cmd/web/routes.go
+++ serving_html/cmd/web/routes.go
@@ -3,16 +3,24 @@ package main
 import (
 	"net/http"
 
-	"github.com/bmizerany/pat"
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
 	"github.com/mbeaver502/LearningGolang_Sawler/serving_html/pkg/config"
 	"github.com/mbeaver502/LearningGolang_Sawler/serving_html/pkg/handlers"
 )
 
 func routes(app *config.AppConfig) http.Handler {
-	mux := pat.New()
+	// Set up routes using pat package
+	//mux := pat.New()
+	//mux.Get("/", http.HandlerFunc(handlers.Repo.Home))
+	//mux.Get("/about", http.HandlerFunc(handlers.Repo.About))
 
-	mux.Get("/", http.HandlerFunc(handlers.Repo.Home))
-	mux.Get("/about", http.HandlerFunc(handlers.Repo.About))
+	mux := chi.NewRouter()
+
+	mux.Use(middleware.Recoverer)
+
+	mux.Get("/", handlers.Repo.Home)
+	mux.Get("/about", handlers.Repo.About)
 
 	return mux
 }
