68a52ff493426cda62e1bd4b30233c21b62beef9
Michael Beaver <jmbeaver1@gmail.com>
2022-07-09 11:36:36 -0500

    Add Base Layout
    Add base layout for HTML templates.
    Update render.RenderTemplate to parse the base layout.

--- serving_html/pkg/render/render.go
+++ serving_html/pkg/render/render.go
@@ -9,9 +9,10 @@ import (
 
 // renderTemplate renders an HTML template to the given http.ResponseWriter.
 func RenderTemplate(w http.ResponseWriter, tmpl string) {
-	path := strings.Join([]string{"./templates/", tmpl}, "")
+	const layoutPath = "./templates/base.layout.tmpl"
+	templatePath := strings.Join([]string{"./templates/", tmpl}, "")
 
-	parsedTemplate, _ := template.ParseFiles(path)
+	parsedTemplate, _ := template.ParseFiles(layoutPath, templatePath)
 	err := parsedTemplate.Execute(w, nil)
 
 	if err != nil {
