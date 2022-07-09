39c80b9ab81bc782c615dc3a4795dce22866f025
Michael Beaver <jmbeaver1@gmail.com>
2022-07-09 13:55:25 -0500

    Demonstrate Session Usage by Saving/Getting IP
    Demonstrate usage of the session manager
    by saving the user's IP address into the session manager
    and then retrieving that value from the session manager.
    The value we load from the session manager
    is then sent as template data to our about page template.

--- serving_html/pkg/handlers/handlers.go
+++ serving_html/pkg/handlers/handlers.go
@@ -31,6 +31,9 @@ func NewHandlers(r *Repository) {
 // Home is the home page handler.
 // Any kind of handler func *must* take in an http.ResponseWriter and a *http.Request!
 func (m *Repository) Home(w http.ResponseWriter, r *http.Request) {
+	// put the user's IP address into our session
+	m.App.Session.Put(r.Context(), "remote_ip", r.RemoteAddr)
+
 	render.RenderTemplate_v3(w, "home.page.tmpl", &models.TemplateData{})
 }
 
@@ -38,6 +41,10 @@ func (m *Repository) Home(w http.ResponseWriter, r *http.Request) {
 func (m *Repository) About(w http.ResponseWriter, r *http.Request) {
 	stringMap := make(map[string]string)
 	stringMap["test"] = "Hello, again."
+
+	// get the user's IP address out of the session ("" if it doesn't exist)
+	// and put it into our stringmap
+	stringMap["remote_ip"] = m.App.Session.GetString(r.Context(), "remote_ip")
 
 	render.RenderTemplate_v3(w, "about.page.tmpl", &models.TemplateData{
 		StringMap: stringMap,
