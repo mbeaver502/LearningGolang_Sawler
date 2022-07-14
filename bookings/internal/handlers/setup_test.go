69c0ff24c48d1bfc85f5b8410fa8d7c1d3657f3f
J. Michael Beaver <jmbeaver1@gmail.com>
2022-07-13 19:00:53 -0500

    Add Dummy Mail Listener for Tests

--- bookings/internal/handlers/setup_test.go
+++ bookings/internal/handlers/setup_test.go
@@ -57,7 +57,20 @@ func TestMain(m *testing.M) {
 	render.NewRenderer(&app)
 	routes(&app)
 
+	mailChan := make(chan models.MailData)
+	app.MailChan = mailChan
+	defer close(app.MailChan)
+	listenForMail()
+
 	os.Exit(m.Run())
+}
+
+func listenForMail() {
+	go func() {
+		for {
+			_ = <-app.MailChan
+		}
+	}()
 }
 
 func routes(app *config.AppConfig) http.Handler {
