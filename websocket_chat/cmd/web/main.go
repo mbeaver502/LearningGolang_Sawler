32e086bd53436e136d3fae0b64c513d08890a605
J. Michael Beaver <jmbeaver1@gmail.com>
2022-07-19 20:06:10 -0500

    Add Basic WebSocket Listeners

--- websocket_chat/cmd/web/main.go
+++ websocket_chat/cmd/web/main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"log"
 	"net/http"
+
+	"github.com/mbeaver502/LearningGolang_Sawler/websocket_chat/internal/handlers"
 )
 
 const SERVER_ADDRESS = ":8080"
@@ -10,7 +12,9 @@ const SERVER_ADDRESS = ":8080"
 func main() {
 	mux := routes()
 
-	log.Println("Starting server on", SERVER_ADDRESS)
+	log.Println("Starting WebSocket channel listener")
+	go handlers.ListenToWsChannel()
 
+	log.Println("Starting server on", SERVER_ADDRESS)
 	_ = http.ListenAndServe(SERVER_ADDRESS, mux)
 }
--- websocket_chat/internal/handlers/handlers.go
+++ websocket_chat/internal/handlers/handlers.go
@@ -1,6 +1,7 @@
 package handlers
 
 import (
+	"fmt"
 	"log"
 	"net/http"
 
@@ -8,11 +9,16 @@ import (
 	"github.com/gorilla/websocket"
 )
 
+var wsChan = make(chan WsPayload)
+var clients = make(map[WebSocketConnection]string)
+
+// views is a collection of jet views.
 var views = jet.NewSet(
 	jet.NewOSFileSystemLoader("./html"),
 	jet.InDevelopmentMode(),
 )
 
+// upgradeConnection is a WebSocket connection upgrader provided by gorilla/websocket.
 var upgradeConnection = websocket.Upgrader{
 	ReadBufferSize:  1024,
 	WriteBufferSize: 1024,
@@ -28,11 +34,24 @@ func Home(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
+// WebSocketConnection is a wrapper for the gorilla/websocket connection.
+type WebSocketConnection struct {
+	*websocket.Conn
+}
+
 // WsJSONResponse defines the JSON response from a WebSocket connection.
 type WsJSONResponse struct {
 	Action      string `json:"action"`
 	Message     string `json:"message"`
 	MessageType string `json:"message_type"`
+}
+
+// WsPayload defines the JSON payload received over the WebSocket connection.
+type WsPayload struct {
+	Action   string              `json:"action"`
+	Username string              `json:"username"`
+	Message  string              `json:"message"`
+	Conn     WebSocketConnection `json:"-"`
 }
 
 // WsEndpoint upgrades connection to WebSocket.
@@ -49,13 +68,65 @@ func WsEndpoint(w http.ResponseWriter, r *http.Request) {
 	var response WsJSONResponse
 	response.Message = `<em><small>Connected to server</small></em>`
 
+	// create a new wrapper for our newly upgraded WebSocket connection
+	conn := WebSocketConnection{
+		Conn: ws,
+	}
+	clients[conn] = ""
+
 	err = ws.WriteJSON(response)
 	if err != nil {
 		log.Println(err)
 		return
 	}
+
+	go ListenForWs(&conn)
 }
 
+func ListenForWs(conn *WebSocketConnection) {
+	defer func() {
+		if r := recover(); r != nil {
+			log.Println("error", fmt.Sprintf("%v", r))
+		}
+	}()
+
+	var payload WsPayload
+
+	for {
+		err := conn.ReadJSON(&payload)
+		if err != nil {
+			// no payload
+		} else {
+			payload.Conn = *conn
+			wsChan <- payload
+		}
+	}
+}
+
+func ListenToWsChannel() {
+	var response WsJSONResponse
+
+	for {
+		e := <-wsChan
+
+		response.Action = "Got Here"
+		response.Message = fmt.Sprintf("Some Message, and action was %s", e.Action)
+		broadcastToAll(response)
+	}
+}
+
+func broadcastToAll(response WsJSONResponse) {
+	for client := range clients {
+		err := client.WriteJSON(response)
+		if err != nil {
+			log.Println("websocket error", err)
+			_ = client.Close()
+			delete(clients, client)
+		}
+	}
+}
+
+// renderPage renders the given template to the given ResponseWriter using the given data.
 func renderPage(w http.ResponseWriter, tmpl string, data jet.VarMap) error {
 	view, err := views.GetTemplate(tmpl)
 	if err != nil {
