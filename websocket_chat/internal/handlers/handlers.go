8ce35311ea182e1f26483527f01832f32984b863
J. Michael Beaver <jmbeaver1@gmail.com>
2022-07-19 19:40:06 -0500

    Add a WebSocket Handler

--- websocket_chat/internal/handlers/handlers.go
+++ websocket_chat/internal/handlers/handlers.go
@@ -5,6 +5,7 @@ import (
 	"net/http"
 
 	"github.com/CloudyKit/jet/v6"
+	"github.com/gorilla/websocket"
 )
 
 var views = jet.NewSet(
@@ -12,9 +13,43 @@ var views = jet.NewSet(
 	jet.InDevelopmentMode(),
 )
 
+var upgradeConnection = websocket.Upgrader{
+	ReadBufferSize:  1024,
+	WriteBufferSize: 1024,
+	CheckOrigin:     func(r *http.Request) bool { return true },
+}
+
 // Home renders the homepage.
 func Home(w http.ResponseWriter, r *http.Request) {
 	err := renderPage(w, "home.jet", nil)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+}
+
+// WsJSONResponse defines the JSON response from a WebSocket connection.
+type WsJSONResponse struct {
+	Action      string `json:"action"`
+	Message     string `json:"message"`
+	MessageType string `json:"message_type"`
+}
+
+// WsEndpoint upgrades connection to WebSocket.
+func WsEndpoint(w http.ResponseWriter, r *http.Request) {
+	// upgrade our HTTP connection to WebSocket
+	ws, err := upgradeConnection.Upgrade(w, r, nil)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+
+	log.Println("client connected to endpoint")
+
+	var response WsJSONResponse
+	response.Message = `<em><small>Connected to server</small></em>`
+
+	err = ws.WriteJSON(response)
 	if err != nil {
 		log.Println(err)
 		return
