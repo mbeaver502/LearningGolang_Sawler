b335768e1550fa643eccdd7b791dd7fb81b4b15b
mbeaver502 <jmbeaver1@gmail.com>
2022-07-25 16:36:14 -0500

    Connect to Postgres

--- backend-app/cmd/api/main.go
+++ backend-app/cmd/api/main.go
@@ -1,12 +1,16 @@
 package main
 
 import (
+	"context"
+	"database/sql"
 	"flag"
 	"fmt"
 	"log"
 	"net/http"
 	"os"
 	"time"
+
+	_ "github.com/lib/pq"
 )
 
 const (
@@ -16,6 +20,9 @@ const (
 type config struct {
 	port int
 	env  string
+	db   struct {
+		dsn string
+	}
 }
 
 type AppStatus struct {
@@ -34,9 +41,17 @@ func main() {
 
 	flag.IntVar(&cfg.port, "port", 4000, "Server port on which to listen")
 	flag.StringVar(&cfg.env, "env", "development", "Application environment (development | production)")
+	flag.StringVar(&cfg.db.dsn, "dsn", "postgres://beaver@localhost/go_movies?sslmode=disable", "Postgres connection string")
 	flag.Parse()
 
 	logger := log.New(os.Stdout, "", log.Ldate|log.Ltime)
+
+	db, err := openDB(cfg)
+	if err != nil {
+		logger.Fatal(err)
+	}
+	defer db.Close()
+
 	app := application{
 		config: cfg,
 		logger: logger,
@@ -52,8 +67,25 @@ func main() {
 
 	app.logger.Println("Starting server on port", app.config.port)
 
-	err := srv.ListenAndServe()
+	err = srv.ListenAndServe()
 	if err != nil {
 		app.logger.Println(err)
 	}
+}
+
+func openDB(cfg config) (*sql.DB, error) {
+	db, err := sql.Open("postgres", cfg.db.dsn)
+	if err != nil {
+		return nil, err
+	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	err = db.PingContext(ctx)
+	if err != nil {
+		return nil, err
+	}
+
+	return db, nil
 }
