6d4d577e29a0bfd3e6297fbd0c70fff8d62833ec
mbeaver502 <jmbeaver1@gmail.com>
2022-07-25 17:56:59 -0500

    Replace Categories => Genres

--- go-movies/src/App.js
+++ go-movies/src/App.js
@@ -3,8 +3,8 @@ import { BrowserRouter as Router, Switch, Route, Link, useRouteMatch } from 'rea
 import Movies from './components/Movies';
 import Home from './components/Home';
 import Admin from './components/Admin';
-import Categories from './components/Categories';
 import OneMovie from './components/OneMovie';
+import Genres from './components/Genres';
 
 export default function App() {
     return (
@@ -29,7 +29,7 @@ export default function App() {
                                     <Link to="/movies">Movies</Link>
                                 </li>
                                 <li className='list-group-item'>
-                                    <Link to="/by-category">Categories</Link>
+                                    <Link to="/genres">Genres</Link>
                                 </li>
                                 <li className='list-group-item'>
                                     <Link to="/admin">Manage Catalog</Link>
@@ -47,20 +47,8 @@ export default function App() {
                             <Route path="/movies">
                                 <Movies />
                             </Route>
-                            <Route exact path="/by-category">
-                                <CategoryPage />
-                            </Route>
-
-                            <Route
-                                exact
-                                path="/by-category/drama"
-                                render={(props) => <Categories {...props} title={`Drama`} />}>
-                            </Route>
-
-                            <Route
-                                exact
-                                path="/by-category/comedy"
-                                render={(props) => <Categories {...props} title={`Comedy`} />}>
+                            <Route exact path="/genres">
+                                <Genres />
                             </Route>
 
                             <Route path="/admin">
@@ -74,20 +62,5 @@ export default function App() {
                 </div>
             </div>
         </Router >
-    );
-}
-
-function CategoryPage() {
-    let { path, url } = useRouteMatch();
-
-    return (
-        <div>
-            <h2>Categories</h2>
-
-            <ul>
-                <li><Link to={`${path}/drama`}>Drama</Link></li>
-                <li><Link to={`${path}/comedy`}>Comedy</Link></li>
-            </ul>
-        </div>
     );
 }
---
+++ go-movies/src/components/Genres.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+export default class Genres extends Component {
+    render() {
+        return (
+            <h2>Genres</h2>
+        );
+    }
+}
