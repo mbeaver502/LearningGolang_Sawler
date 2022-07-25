c2a6caad206916da4cb5ae31bcbeb5ebe10882cd
mbeaver502 <jmbeaver1@gmail.com>
2022-07-24 21:03:43 -0500

    Create New Components, Add New Routing

--- go-movies/src/App.js
+++ go-movies/src/App.js
@@ -1,8 +1,10 @@
 import React, { Fragment } from 'react';
-import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route, Link, useParams, useRouteMatch } from 'react-router-dom';
 import Movies from './components/Movies';
 import Home from './components/Home';
 import Admin from './components/Admin';
+import Categories from './components/Categories';
+import OneMovie from './components/OneMovie';
 
 export default function App() {
     return (
@@ -27,6 +29,9 @@ export default function App() {
                                     <Link to="/movies">Movies</Link>
                                 </li>
                                 <li className='list-group-item'>
+                                    <Link to="/by-category">Categories</Link>
+                                </li>
+                                <li className='list-group-item'>
                                     <Link to="/admin">Manage Catalog</Link>
                                 </li>
                             </ul>
@@ -35,9 +40,29 @@ export default function App() {
 
                     <div className='col-md-10'>
                         <Switch>
+
+                            <Route path="/movies/:id"
+                                component={OneMovie} />
+
                             <Route path="/movies">
                                 <Movies />
                             </Route>
+                            <Route exact path="/by-category">
+                                <CategoryPage />
+                            </Route>
+
+                            <Route
+                                exact
+                                path="/by-category/drama"
+                                render={(props) => <Categories {...props} title={`Drama`} />}>
+                            </Route>
+
+                            <Route
+                                exact
+                                path="/by-category/comedy"
+                                render={(props) => <Categories {...props} title={`Comedy`} />}>
+                            </Route>
+
                             <Route path="/admin">
                                 <Admin />
                             </Route>
@@ -48,6 +73,21 @@ export default function App() {
                     </div>
                 </div>
             </div>
-        </Router>
+        </Router >
+    );
+}
+
+function CategoryPage() {
+    let { path, url } = useRouteMatch();
+
+    return (
+        <div>
+            <h2>Categories</h2>
+
+            <ul>
+                <li><Link to={`${path}/drama`}>Drama</Link></li>
+                <li><Link to={`${path}/comedy`}>Comedy</Link></li>
+            </ul>
+        </div>
     );
 }
