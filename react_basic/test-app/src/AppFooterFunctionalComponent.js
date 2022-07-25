2c093eec36177373ac76c886aab6991f1a4815c8
mbeaver502 <jmbeaver1@gmail.com>
2022-07-24 19:48:55 -0500

    Hello World App in React
    Basic Hello World app created using
    create-react-app function.

---
+++ react_basic/test-app/src/AppFooterFunctionalComponent.js
@@ -0,0 +1,16 @@
+import React, { Fragment } from 'react';
+import './AppFooter.css';
+
+export default function AppFooterFunctionalComponent(props) {
+    const currentYear = new Date().getFullYear();
+    return (
+        <Fragment>
+            <hr />
+            <p className='footer'>
+                Copyright &copy; 2020 - {currentYear} Acme Ltd.
+                <br />
+                {props.myProperty}
+            </p>
+        </Fragment>
+    );
+}
---
+++ react_basic/test-app/src/index.js
@@ -0,0 +1,42 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+// import AppFooter from './AppFooter';
+import AppContent from './AppContent';
+import AppHeader from './AppHeader';
+import AppFooterFunctionalComponent from './AppFooterFunctionalComponent';
+
+import './index.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+
+class App extends Component {
+
+    constructor(props) {
+        super(props)
+        this.handlePostChange = this.handlePostChange.bind(this);
+        this.state = { posts: [] }
+    }
+
+    handlePostChange = (posts) => {
+        this.setState({posts: posts});
+    }
+
+    render() {
+        const myProps = {
+            title: "My Cool App!",
+            subject: "My Subject",
+            favorite_color: "red"
+        }
+
+        return (
+            <div className='app'>
+                <AppHeader {...myProps} posts={this.state.posts} handlePostChange={this.handlePostChange} />
+                <AppContent handlePostChange={this.handlePostChange} posts={this.state.posts}/>
+                {/* <AppFooter /> */}
+                <AppFooterFunctionalComponent myProperty={"Hello, World!"}/>
+            </div>
+        );
+    }
+}
+
+ReactDOM.render(<App />, document.getElementById('root'));
