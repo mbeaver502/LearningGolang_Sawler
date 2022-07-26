044c9ba5a192cde6b117e04336a0873ea0d54d27
mbeaver502 <jmbeaver1@gmail.com>
2022-07-26 16:59:54 -0500

    Add Functionality to Update a Movie
    When editing or adding a movie,
    do not automatically insert a new DB record.
    Instead, first try to get the movie from the DB
    and then try to update if it exists. Else, insert.

--- backend-app/cmd/api/movie_handlers.go
+++ backend-app/cmd/api/movie_handlers.go
@@ -132,6 +132,13 @@ func (app *application) editMovie(w http.ResponseWriter, r *http.Request) {
 
 	var movie models.Movie
 
+	if payload.ID != "0" {
+		id, _ := strconv.Atoi(payload.ID)
+		m, _ := app.models.DB.Get(id)
+		movie = *m
+		movie.UpdatedAt = time.Now()
+	}
+
 	movie.ID, _ = strconv.Atoi(payload.ID)
 	movie.Title = payload.Title
 	movie.Description = payload.Description
@@ -143,11 +150,20 @@ func (app *application) editMovie(w http.ResponseWriter, r *http.Request) {
 	movie.CreatedAt = time.Now()
 	movie.UpdatedAt = time.Now()
 
-	err = app.models.DB.InsertMovie(movie)
-	if err != nil {
-		app.logger.Println(err)
-		app.errorJSON(w, err)
-		return
+	if movie.ID == 0 {
+		err = app.models.DB.InsertMovie(movie)
+		if err != nil {
+			app.logger.Println(err)
+			app.errorJSON(w, err)
+			return
+		}
+	} else {
+		err = app.models.DB.UpdateMovie(movie)
+		if err != nil {
+			app.logger.Println(err)
+			app.errorJSON(w, err)
+			return
+		}
 	}
 
 	ok := jsonResp{
