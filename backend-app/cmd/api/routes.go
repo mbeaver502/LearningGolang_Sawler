70faf37768c240d541d04e43d286720841b4b2a3
mbeaver502 <jmbeaver1@gmail.com>
2022-07-27 18:37:20 -0500

    Rename GraphQL Endpoint

--- backend-app/cmd/api/routes.go
+++ backend-app/cmd/api/routes.go
@@ -26,7 +26,7 @@ func (app *application) routes() http.Handler {
 	router.POST("/v1/admin/editmovie", app.wrap(secure.ThenFunc(app.editMovie)))
 	router.GET("/v1/admin/deletemovie/:id", app.wrap(secure.ThenFunc(app.deleteMovie)))
 
-	router.HandlerFunc(http.MethodPost, "/v1/graphql/list", app.moviesGraphQL)
+	router.HandlerFunc(http.MethodPost, "/v1/graphql", app.moviesGraphQL)
 
 	return app.enableCORS(router)
 }
