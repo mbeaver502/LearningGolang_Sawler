5a1040b43893a7cfb0a5579bdcaa05d349fe5e20
mbeaver502 <jmbeaver1@gmail.com>
2022-07-28 16:54:51 -0500

    Create Vue App
    Add components for a page header,
    page body, and page footer.

---
+++ vue_books/vue_frontend/src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <Header />
+  <Body />
+  <Footer />
+</template>
+
+<script>
+import PageHeader from "./components/PageHeader.vue";
+import PageBody from "./components/PageBody.vue";
+import PageFooter from "./components/PageFooter.vue";
+
+export default {
+  name: "App",
+  components: {
+    Header: PageHeader,
+    Body: PageBody,
+    Footer: PageFooter,
+  },
+};
+</script>
+
+<style>
+</style>
---
+++ vue_books/vue_frontend/src/components/PageBody.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="container">
+    <div class="row">
+      <div class="col">
+        <img
+          alt="book and glasses"
+          src="./../assets/book-and-glasses.png"
+          class="img-fluid mt-5"
+        />
+      </div>
+    </div>
+  </div>
+</template>
