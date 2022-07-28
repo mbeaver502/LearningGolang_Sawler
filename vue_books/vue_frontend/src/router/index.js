31f9ac7b1b6b30763ed380e61d2139cf35abe681
mbeaver502 <jmbeaver1@gmail.com>
2022-07-28 18:35:44 -0500

    Add Route for /login
    Create basic login component.
    Add route to /login using new component.
    Update main navbar component.

--- vue_books/vue_frontend/src/router/index.js
+++ vue_books/vue_frontend/src/router/index.js
@@ -1,12 +1,18 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import PageBody from './../components/PageBody.vue';
+import UserLogin from '../components/UserLogin.vue';
 
 const routes = [
     {
         path: '/',
         name: 'Home',
         component: PageBody,
-    }
+    },
+    {
+        path: '/login',
+        name: 'Login',
+        component: UserLogin,
+    },
 ];
 
 const router = createRouter({ history: createWebHistory(), routes });
