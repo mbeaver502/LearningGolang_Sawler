cd471ca091e8fc9bc18a480f62c9f6efe0ef9752
mbeaver502 <jmbeaver1@gmail.com>
2022-07-28 21:00:08 -0500

    Add CORS Policy

--- vue_books/go_backend/cmd/api/routes.go
+++ vue_books/go_backend/cmd/api/routes.go
@@ -5,12 +5,21 @@ import (
 
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
+	"github.com/go-chi/cors"
 )
 
 func (app *application) routes() http.Handler {
 	mux := chi.NewRouter()
 
 	mux.Use(middleware.Recoverer)
+	mux.Use(cors.Handler(cors.Options{
+		AllowedOrigins:   []string{"https://*", "http://*"},
+		AllowedMethods:   []string{"POST", "GET"},
+		AllowedHeaders:   []string{"Accept", "Authorization", "Content-Type", "X-CSRF-Token"},
+		ExposedHeaders:   []string{"Link"},
+		AllowCredentials: true,
+		MaxAge:           300,
+	}))
 
 	mux.Get("/users/login", app.Login)
 	mux.Post("/users/login", app.Login)
