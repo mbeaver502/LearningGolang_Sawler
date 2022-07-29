2042ee45134a4b5e797cb2da930534a62d27cbe5
mbeaver502 <jmbeaver1@gmail.com>
2022-07-28 19:04:26 -0500

    Create Basic Form Components

---
+++ vue_books/vue_frontend/src/components/forms/CheckboxInput.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="form-check">
+    <input
+      class="form-check-input"
+      :required="required"
+      :name="name"
+      :value="value"
+      :id="name"
+      :checked="checked"
+      type="checkbox"
+    />
+    <label :for="name" class="form-check-label">{{ label }}</label>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "CheckboxInput",
+  props: ["label", "name", "value", "required", "checked"],
+};
+</script>
---
+++ vue_books/vue_frontend/src/components/forms/FormTag.vue
@@ -0,0 +1,42 @@
+<template>
+  <form
+    :action="action"
+    autocomplete="off"
+    :method="method"
+    class="needs-validation"
+    novalidate
+  >
+    <slot></slot>
+  </form>
+</template>
+
+<script>
+export default {
+  name: "FormTag",
+  props: ["method", "action"],
+  mounted() {
+    (function () {
+      "use strict";
+
+      // Fetch all the forms we want to apply custom Bootstrap validation styles to
+      var forms = document.querySelectorAll(".needs-validation");
+
+      // Loop over them and prevent submission
+      Array.prototype.slice.call(forms).forEach(function (form) {
+        form.addEventListener(
+          "submit",
+          function (event) {
+            if (!form.checkValidity()) {
+              event.preventDefault();
+              event.stopPropagation();
+            }
+
+            form.classList.add("was-validated");
+          },
+          false
+        );
+      });
+    })();
+  },
+};
+</script>
