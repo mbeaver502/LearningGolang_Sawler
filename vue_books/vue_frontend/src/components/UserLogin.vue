c7f3cd0a2b75b1086bd577414da9bd46d5ff9d6e
mbeaver502 <jmbeaver1@gmail.com>
2022-07-28 19:04:43 -0500

    Update Login Template to Use New Components

--- vue_books/vue_frontend/src/components/UserLogin.vue
+++ vue_books/vue_frontend/src/components/UserLogin.vue
@@ -1,3 +1,39 @@
 <template>
-  <p>Login</p>
+  <div class="container">
+    <div class="row">
+      <div class="col">
+        <h1 class="mt-5">Login</h1>
+        <hr />
+        <form-tag>
+          <text-input label="Email" type="email" name="email" required="true">
+          </text-input>
+
+          <text-input
+            label="Password"
+            type="password"
+            name="password"
+            required="true"
+          >
+          </text-input>
+
+          <hr />
+          <input type="submit" class="btn btn-primary" value="Login" />
+        </form-tag>
+      </div>
+    </div>
+  </div>
 </template>
+
+<script>
+import TextInput from "./forms/TextInput.vue";
+import FormTag from "./forms/FormTag.vue";
+
+export default {
+  name: "UserLogin",
+  components: {
+    TextInput,
+    FormTag,
+  },
+  mounted() {},
+};
+</script>
