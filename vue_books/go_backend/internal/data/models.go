c3d4a14a19d1692da7198837abe4b00ea33659b5
mbeaver502 <jmbeaver1@gmail.com>
2022-07-29 10:57:23 -0500

    Add CRUD Functions for User Type
    Use the User as a receiver to add
    basic CRUD operations for user DB table.
    Add helper method for checking password matches.

--- vue_books/go_backend/internal/data/models.go
+++ vue_books/go_backend/internal/data/models.go
@@ -3,7 +3,10 @@ package data
 import (
 	"context"
 	"database/sql"
+	"errors"
 	"time"
+
+	"golang.org/x/crypto/bcrypt"
 )
 
 const dbTimeout = 3 * time.Second
@@ -38,6 +41,7 @@ type User struct {
 	Token     Token     `json:"token"`
 }
 
+// GetAll gets all users.
 func (u *User) GetAll() ([]*User, error) {
 	ctx, cancel := context.WithTimeout(context.Background(), dbTimeout)
 	defer cancel()
@@ -75,6 +79,172 @@ func (u *User) GetAll() ([]*User, error) {
 	}
 
 	return users, nil
+}
+
+// GetByID gets a specific user by the given ID.
+func (u *User) GetByID(id int) (*User, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), dbTimeout)
+	defer cancel()
+
+	query := `select id, email, first_name, last_name, password, created_at, updated_at from users where id = $1`
+
+	var user User
+
+	row := db.QueryRowContext(ctx, query, id)
+	err := row.Scan(
+		&user.ID,
+		&user.Email,
+		&user.FirstName,
+		&user.LastName,
+		&user.Password,
+		&user.CreatedAt,
+		&user.UpdatedAt,
+	)
+	if err != nil {
+		return nil, err
+	}
+
+	return &user, nil
+}
+
+// GetByEmail gets a specific user by the given email address.
+func (u *User) GetByEmail(email string) (*User, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), dbTimeout)
+	defer cancel()
+
+	query := `select id, email, first_name, last_name, password, created_at, updated_at from users where email = $1`
+
+	var user User
+
+	row := db.QueryRowContext(ctx, query, email)
+	err := row.Scan(
+		&user.ID,
+		&user.Email,
+		&user.FirstName,
+		&user.LastName,
+		&user.Password,
+		&user.CreatedAt,
+		&user.UpdatedAt,
+	)
+	if err != nil {
+		return nil, err
+	}
+
+	return &user, nil
+}
+
+// Update updates the user.
+func (u *User) Update() error {
+	ctx, cancel := context.WithTimeout(context.Background(), dbTimeout)
+	defer cancel()
+
+	stmt := `update users
+		set email = $1,
+			first_name = $2,
+			last_name = $3,
+			updated_at = $4
+		where id = $5`
+
+	_, err := db.ExecContext(ctx, stmt,
+		u.Email,
+		u.FirstName,
+		u.LastName,
+		time.Now(),
+		u.ID,
+	)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// Delete deletes the user.
+func (u *User) Delete() error {
+	ctx, cancel := context.WithTimeout(context.Background(), dbTimeout)
+	defer cancel()
+
+	stmt := `delete from users where id = $1`
+
+	_, err := db.ExecContext(ctx, stmt, u.ID)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// Insert inserts a user and returns the new ID.
+func (u *User) Insert() (int, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), dbTimeout)
+	defer cancel()
+
+	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(u.Password), 12)
+	if err != nil {
+		return 0, err
+	}
+
+	var newID int
+
+	stmt := `insert into 
+		users (email, first_name, last_name, password, created_at, updated_at)
+		values ($1, $2, $3, $4, $5, $6) 
+		returning id`
+
+	err = db.QueryRowContext(ctx, stmt,
+		u.Email,
+		u.FirstName,
+		u.LastName,
+		hashedPassword,
+		time.Now(),
+		time.Now(),
+	).Scan(&newID)
+
+	if err != nil {
+		return 0, err
+	}
+
+	return newID, nil
+}
+
+// ResetPassword resets a user's password.
+func (u *User) ResetPassword(password string) error {
+	ctx, cancel := context.WithTimeout(context.Background(), dbTimeout)
+	defer cancel()
+
+	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(password), 12)
+	if err != nil {
+		return err
+	}
+
+	stmt := `update users
+		set password = $1
+		where id = $2`
+
+	_, err = db.ExecContext(ctx, stmt,
+		hashedPassword,
+		u.ID,
+	)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// PasswordMatches determines whether a given plaintext password matches the user's hashed password.
+func (u *User) PasswordMatches(plaintext string) (bool, error) {
+	err := bcrypt.CompareHashAndPassword([]byte(u.Password), []byte(plaintext))
+	if err != nil {
+		switch {
+		case errors.Is(err, bcrypt.ErrMismatchedHashAndPassword):
+			return false, nil
+		default:
+			return false, err
+		}
+	}
+
+	return true, nil
 }
 
 // Token is a token for a given user.
