aed9b552e8ad648d95da65fa2b9075c54f78a6c2
mbeaver502 <jmbeaver1@gmail.com>
2022-07-29 14:27:08 -0500

    Add Stub Components and Routes

--- vue_books/vue_frontend/src/router/index.js
+++ vue_books/vue_frontend/src/router/index.js
@@ -1,6 +1,12 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import PageBody from './../components/PageBody.vue';
 import UserLogin from '../components/UserLogin.vue';
+import BooksView from '../components/BooksView.vue';
+import BookView from '../components/BookView.vue';
+import BooksAdmin from './../components/BooksAdmin.vue';
+import BookEdit from './../components/BookEdit.vue';
+import UsersView from '../components/UsersView.vue';
+import UserEdit from './../components/UserEdit.vue';
 
 const routes = [
     {
@@ -12,6 +18,36 @@ const routes = [
         path: '/login',
         name: 'Login',
         component: UserLogin,
+    },
+    {
+        path: '/books',
+        name: 'Books',
+        component: BooksView,
+    },
+    {
+        path: '/books/:bookName',
+        name: 'Book',
+        component: BookView,
+    },
+    {
+        path: '/admin/books',
+        name: 'Books Admin',
+        component: BooksAdmin,
+    },
+    {
+        path: '/admin/books/:bookId',
+        name: 'Book Edit',
+        component: BookEdit,
+    },
+    {
+        path: '/admin/users',
+        name: 'Users',
+        component: UsersView,
+    },
+    {
+        path: '/admin/users/:userId',
+        name: 'User Edit',
+        component: UserEdit,
     },
 ];
 
