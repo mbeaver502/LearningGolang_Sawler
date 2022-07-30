53abdb54d3050022ca7acb97d4f5f5bbcc74e815
mbeaver502 <jmbeaver1@gmail.com>
2022-07-30 10:29:32 -0500

    Add Filter Tags, Transition Animation, and Styles

--- vue_books/vue_frontend/src/components/BooksView.vue
+++ vue_books/vue_frontend/src/components/BooksView.vue
@@ -7,9 +7,56 @@
 
       <hr />
 
+      <div class="filters text-center">
+        <span
+          class="filter me-1"
+          v-bind:class="{ active: currentFilter === 0 }"
+          v-on:click="setFilter(0)"
+          >ALL</span
+        >
+        <span
+          class="filter me-1"
+          v-bind:class="{ active: currentFilter === 7 }"
+          v-on:click="setFilter(7)"
+          >CLASSIC</span
+        >
+        <span
+          class="filter me-1"
+          v-bind:class="{ active: currentFilter === 2 }"
+          v-on:click="setFilter(2)"
+          >FANTASY</span
+        >
+        <span
+          class="filter me-1"
+          v-bind:class="{ active: currentFilter === 6 }"
+          v-on:click="setFilter(6)"
+          >HORROR</span
+        >
+        <span
+          class="filter me-1"
+          v-bind:class="{ active: currentFilter === 4 }"
+          v-on:click="setFilter(4)"
+          >THRILLER</span
+        >
+        <span
+          class="filter me-1"
+          v-bind:class="{ active: currentFilter === 1 }"
+          v-on:click="setFilter(1)"
+          >SCIENCE FICTION</span
+        >
+      </div>
+
+      <hr />
+
       <div>
         <div class="card-group">
-          <div class="p-3 d-flex flex-wrap">
+          <!-- <div class="p-3 d-flex flex-wrap"> -->
+          <transition-group
+            class="p-3 d-flex flex-wrap"
+            tag="div"
+            appear
+            name="books"
+          >
             <div v-for="b in this.books" :key="b.id">
               <div
                 class="card me-2 ms-1 mb-3"
@@ -25,11 +72,24 @@
                 />
                 <div class="card-body text-center">
                   <h6 class="card-title">{{ b.title }}</h6>
-                  {{ b.author.author_name }}
+                  <span class="book-author">{{ b.author.author_name }}</span
+                  ><br />
+                  <small
+                    class="text-muted book-genre"
+                    v-for="(g, index) in b.genres"
+                    v-bind:key="g.id"
+                  >
+                    <em class="me-1"
+                      >{{ g.genre_name
+                      }}<template v-if="index !== b.genres.length - 1"
+                        >,</template
+                      >
+                    </em>
+                  </small>
                 </div>
               </div>
             </div>
-          </div>
+          </transition-group>
         </div>
       </div>
     </div>
@@ -65,6 +125,54 @@ export default {
         this.$emit("error", error);
       });
   },
-  methods: {},
+  methods: {
+    setFilter: function (filter) {
+      this.currentFilter = filter;
+    },
+  },
 };
 </script>
+
+<style scoped>
+.book-author,
+.book-genre {
+  font-size: 0.8em;
+}
+
+.filters {
+  height: 2.5em;
+}
+.filter {
+  padding: 6px 6px;
+  cursor: pointer;
+  border-radius: 6px;
+  transition: all 0.35s;
+  border: 1px solid silver;
+}
+
+.filter.active {
+  background: lightgreen;
+}
+
+.filter:hover {
+  background: lightgray;
+}
+
+/* transition styles */
+.books-move {
+  transition: all 500ms ease-in-out 50ms;
+}
+
+.books-enter-active {
+  transition: all 500ms ease-in-out;
+}
+
+.books-leave-active {
+  transition: all 500ms ease-in;
+}
+
+.books-enter,
+.books-leave-to {
+  opacity: 0;
+}
+</style>
