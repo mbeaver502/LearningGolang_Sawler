8fbf0272b550fce12588b581ea001a87e25dc869
mbeaver502 <jmbeaver1@gmail.com>
2022-07-31 10:55:23 -0500

    Add Handler and Route for Sending Mail

---
+++ microservices/mail_service/cmd/api/handlers.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func (app *Config) SendMail(w http.ResponseWriter, r *http.Request) {
+	// what we expect to receive in our request
+	type mailMessage struct {
+		From    string `json:"from"`
+		To      string `json:"to"`
+		Subject string `json:"subject"`
+		Message string `json:"message"`
+	}
+
+	var requestPayload mailMessage
+
+	// attempt to parse the request
+	err := app.readJSON(w, r, &requestPayload)
+	if err != nil {
+		log.Println(err)
+		app.errorJSON(w, err)
+		return
+	}
+
+	// create our message
+	msg := Message{
+		From:    requestPayload.From,
+		To:      requestPayload.To,
+		Subject: requestPayload.Subject,
+		Data:    requestPayload.Message,
+	}
+
+	// try to send the email
+	err = app.Mailer.SendSMTPMessage(msg)
+	if err != nil {
+		log.Println(err)
+		app.errorJSON(w, err)
+		return
+	}
+
+	// write back to the caller
+	payload := jsonResponse{
+		Error:   false,
+		Message: fmt.Sprintf("email sent to %s", requestPayload.To),
+	}
+
+	app.writeJSON(w, http.StatusOK, payload)
+}
--- microservices/mail_service/cmd/api/main.go
+++ microservices/mail_service/cmd/api/main.go
@@ -4,17 +4,22 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"os"
+	"strconv"
 )
 
 const webPort = "80"
 
 type Config struct {
+	Mailer Mail
 }
 
 func main() {
 	log.Printf("Mail listening on port %s\n", webPort)
 
-	app := Config{}
+	app := Config{
+		Mailer: createMail(),
+	}
 
 	srv := &http.Server{
 		Addr:    fmt.Sprintf(":%s", webPort),
@@ -24,5 +29,23 @@ func main() {
 	err := srv.ListenAndServe()
 	if err != nil {
 		log.Panic(err)
+	}
+}
+
+func createMail() Mail {
+	port, err := strconv.Atoi(os.Getenv("MAIL_PORT"))
+	if err != nil {
+		log.Panic(err)
+	}
+
+	return Mail{
+		Domain:      os.Getenv("MAIL_DOMAIN"),
+		Host:        os.Getenv("MAIL_HOST"),
+		Port:        port,
+		Username:    os.Getenv("MAIL_USERNAME"),
+		Password:    os.Getenv("MAIL_PASSWORD"),
+		Encryption:  os.Getenv("MAIL_ENCRYPTION"),
+		FromAddress: os.Getenv("MAIL_FROM_NAME"),
+		FromName:    os.Getenv("MAIL_FROM_ADDRESS"),
 	}
 }
