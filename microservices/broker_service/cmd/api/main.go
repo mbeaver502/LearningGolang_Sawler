cbf1ccf40acb77d0811d8d1b3180ec2b1b3e0e21
mbeaver502 <jmbeaver1@gmail.com>
2022-07-30 19:28:18 -0500

    Scaffold Broker Service

---
+++ microservices/broker_service/cmd/api/main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+const webPort = "80"
+
+type Config struct {
+}
+
+func main() {
+	app := Config{}
+
+	log.Printf("Broker listening on port %s\n", webPort)
+
+	// define an HTTP server
+	srv := &http.Server{
+		Addr:    fmt.Sprintf(":%s", webPort),
+		Handler: app.routes(),
+	}
+
+	// start the server
+	err := srv.ListenAndServe()
+	if err != nil {
+		log.Panic(err)
+	}
+}
