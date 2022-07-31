904404ff4d1c46624e5604e46584182dca950c6e
mbeaver502 <jmbeaver1@gmail.com>
2022-07-30 21:22:23 -0500

    Add Default Route and Handler
    The default Route/Handler will act as
    a kind of multiplexer based on the
    incoming request's "action" field.
    For the "auth" action, attempt to
    call the authentication service
    to authenticate the given credentials.

--- microservices/broker_service/cmd/api/handlers.go
+++ microservices/broker_service/cmd/api/handlers.go
@@ -1,10 +1,26 @@
 package main
 
 import (
+	"bytes"
+	"encoding/json"
+	"errors"
+	"log"
 	"net/http"
 )
 
-// Broker is the default handler.
+// RequestPayload is the standard, expected JSON structure.
+type RequestPayload struct {
+	Action string      `json:"action"`
+	Auth   AuthPayload `json:"auth,omitempty"`
+}
+
+// AuthPayload represents the JSON for an authentication request.
+type AuthPayload struct {
+	Email    string `json:"email"`
+	Password string `json:"password"`
+}
+
+// Broker is a sample handler.
 func (app *Config) Broker(w http.ResponseWriter, r *http.Request) {
 	payload := jsonResponse{
 		Error:   false,
@@ -13,4 +29,80 @@ func (app *Config) Broker(w http.ResponseWriter, r *http.Request) {
 	}
 
 	_ = app.writeJSON(w, http.StatusOK, payload)
+}
+
+// HandleSubmission is the default handler for all requests.
+func (app *Config) HandleSubmission(w http.ResponseWriter, r *http.Request) {
+	var requestPayload RequestPayload
+
+	// attempt to parse the request
+	err := app.readJSON(w, r, &requestPayload)
+	if err != nil {
+		log.Println(err)
+		app.errorJSON(w, err)
+		return
+	}
+
+	// act according to what the user requested
+	switch requestPayload.Action {
+	case "auth":
+		app.authenticate(w, requestPayload.Auth)
+	default:
+		app.errorJSON(w, errors.New("unrecognized action"))
+	}
+}
+
+func (app *Config) authenticate(w http.ResponseWriter, a AuthPayload) {
+	// create some JSON to be sent to the auth service
+	jsonData, _ := json.Marshal(a)
+
+	// call the auth service -- using the host service we named in docker-compose
+	req, err := http.NewRequest("POST", "http://authentication-service/authenticate", bytes.NewBuffer(jsonData))
+	if err != nil {
+		log.Println(err)
+		app.errorJSON(w, err)
+		return
+	}
+
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		log.Println(err)
+		app.errorJSON(w, err)
+		return
+	}
+	defer resp.Body.Close()
+
+	// make sure we get back the correct status code
+	if resp.StatusCode == http.StatusUnauthorized {
+		app.errorJSON(w, errors.New("invalid credentials"))
+		return
+	} else if resp.StatusCode != http.StatusAccepted {
+		app.errorJSON(w, errors.New("error calling auth service"))
+		return
+	}
+
+	// create a variable to read resp.Body into
+	var jsonFromService jsonResponse
+
+	// decode the JSON from the auth service
+	err = json.NewDecoder(resp.Body).Decode(&jsonFromService)
+	if err != nil {
+		log.Println(err)
+		app.errorJSON(w, err)
+		return
+	}
+
+	// if auth service said there's an error, tell user
+	if jsonFromService.Error {
+		app.errorJSON(w, errors.New(jsonFromService.Message), http.StatusUnauthorized)
+	}
+
+	payload := jsonResponse{
+		Error:   false,
+		Message: "authenticated",
+		Data:    jsonFromService.Data,
+	}
+
+	app.writeJSON(w, http.StatusAccepted, payload)
 }
