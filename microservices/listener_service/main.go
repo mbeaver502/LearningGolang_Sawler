40023dc2670361073437fc93416bd639213ff59e
mbeaver502 <jmbeaver1@gmail.com>
2022-07-31 11:38:36 -0500

    Scaffold Listener Service

---
+++ microservices/listener_service/main.go
@@ -0,0 +1,15 @@
+package main
+
+import "log"
+
+func main() {
+	log.Println("Listener started")
+
+	// try to connect to rabbitmq
+
+	// start listening for messages
+
+	// create consumer
+
+	// watch the queue and consume events
+}
