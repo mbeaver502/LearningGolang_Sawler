db5c61f35b1f17d955abc28afadff8f9e1bf27e1
mbeaver502 <jmbeaver1@gmail.com>
2022-08-01 19:28:03 -0500

    Add Open, Current, Change Prices to Main Window
    Call the API to get current gold price data.
    Parse that JSON data into three canvas
    text blocks, and place those three text
    blocks into their own container.
    Add that new container to the app's
    main window for display.

---
+++ fyne/goldwatcher/price_text.go
@@ -0,0 +1,63 @@
+package main
+
+import (
+	"fmt"
+	"image/color"
+
+	"fyne.io/fyne/v2"
+	"fyne.io/fyne/v2/canvas"
+)
+
+func (app *Config) getPriceText() (*canvas.Text, *canvas.Text, *canvas.Text) {
+	var g Gold
+	var open, current, change *canvas.Text
+
+	p, err := g.GetPrices()
+	if err != nil {
+		app.ErrorLog.Println(err)
+
+		gray := color.NRGBA{
+			R: 155,
+			G: 155,
+			B: 155,
+			A: 255,
+		}
+
+		open = canvas.NewText("Open: Unreachable", gray)
+		current = canvas.NewText("Current: Unreachable", gray)
+		change = canvas.NewText("Change: Unreachable", gray)
+
+		return open, current, change
+	}
+
+	// default display color
+	displayColor := color.NRGBA{
+		R: 0,
+		G: 180,
+		B: 0,
+		A: 255,
+	}
+
+	if p.Price < p.PreviousClose {
+		displayColor = color.NRGBA{
+			R: 180,
+			G: 0,
+			B: 0,
+			A: 255,
+		}
+	}
+
+	openText := fmt.Sprintf("Open: $%.4f %s", p.PreviousClose, currency)
+	currentText := fmt.Sprintf("Current: $%.4f %s", p.Price, currency)
+	changeText := fmt.Sprintf("Change: $%.4f %s", p.ChangeInPrice, currency)
+
+	open = canvas.NewText(openText, nil)
+	current = canvas.NewText(currentText, displayColor)
+	change = canvas.NewText(changeText, displayColor)
+
+	open.Alignment = fyne.TextAlignLeading
+	current.Alignment = fyne.TextAlignCenter
+	change.Alignment = fyne.TextAlignTrailing
+
+	return open, current, change
+}
--- fyne/goldwatcher/ui.go
+++ fyne/goldwatcher/ui.go
@@ -1,9 +1,16 @@
 package main
 
+import "fyne.io/fyne/v2/container"
+
 func (app *Config) makeUI() {
 	// get current price of gold
+	openPrice, currentPrice, priceChange := app.getPriceText()
 
 	// put price information into container
+	priceContent := container.NewGridWithColumns(3, openPrice, currentPrice, priceChange)
+	app.PriceContainer = priceContent
 
 	// add container to window
+	finalContent := container.NewVBox(priceContent)
+	app.MainWindow.SetContent(finalContent)
 }
