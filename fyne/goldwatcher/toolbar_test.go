ee87f2f1d1186602ae56eb70967630e190d5ceda
mbeaver502 <jmbeaver1@gmail.com>
2022-08-02 20:40:35 -0500

    Add Simple Test for addHoldingsDialog
    Just testing whether the text entries
    are working.

--- fyne/goldwatcher/toolbar_test.go
+++ fyne/goldwatcher/toolbar_test.go
@@ -1,10 +1,34 @@
 package main
 
-import "testing"
+import (
+	"testing"
+
+	"fyne.io/fyne/v2/test"
+)
 
 func TestConfig_getToolbar(t *testing.T) {
 	tb := testApp.getToolbar()
 	if len(tb.Items) != 4 {
 		t.Errorf("toolbar items: got %d; want %d", len(tb.Items), 4)
+	}
+}
+
+func TestConfig_addHoldingsDialog(t *testing.T) {
+	_ = testApp.addHoldingsDialog()
+
+	test.Type(testApp.AddHoldingsPurchaseAmountEntry, "1")
+	test.Type(testApp.AddHoldingsPurchasePriceEntry, "1234")
+	test.Type(testApp.AddHoldingsPurchaseDateEntry, "2022-08-02")
+
+	if testApp.AddHoldingsPurchaseDateEntry.Text != "2022-08-02" {
+		t.Errorf("got %s; want %s", testApp.AddHoldingsPurchaseDateEntry.Text, "2022-08-02")
+	}
+
+	if testApp.AddHoldingsPurchasePriceEntry.Text != "1234" {
+		t.Errorf("got %s; want %s", testApp.AddHoldingsPurchasePriceEntry.Text, "1234")
+	}
+
+	if testApp.AddHoldingsPurchaseAmountEntry.Text != "1" {
+		t.Errorf("got %s; want %s", testApp.AddHoldingsPurchaseAmountEntry.Text, "1")
 	}
 }
