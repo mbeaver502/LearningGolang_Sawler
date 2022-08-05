24b374a69c9cdbeef4c359595eeba81df2d5051b
mbeaver502 <jmbeaver1@gmail.com>
2022-08-05 15:46:04 -0500

    Simple Mutex to Safely Access Memory

--- go_concurrency/mutex/main.go
+++ go_concurrency/mutex/main.go
@@ -11,17 +11,22 @@ var wg sync.WaitGroup
 func main() {
 	msg = "Hello, world!"
 
+	var mutex sync.Mutex
+
 	// data race! both goroutines access and write to same location
 	wg.Add(2)
-	go updateMessage("Hello, universe!")
-	go updateMessage("Hello, cosmos!")
+	go updateMessage("Hello, universe!", &mutex)
+	go updateMessage("Hello, cosmos!", &mutex)
 	wg.Wait()
 
 	fmt.Println(msg)
 }
 
-func updateMessage(s string) {
+func updateMessage(s string, m *sync.Mutex) {
 	defer wg.Done()
 
+	// mutex allows us to safely access and write msg
+	m.Lock()
 	msg = s
+	m.Unlock()
 }
